dc142eb11bc4fb41e937cbf1f4f3fb6976fa4331
Puneet kumar <143442578+PuneetKumar747@users.noreply.github.com>
2024-09-12 10:54:50 +0530

    update email formate

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def authorize():
 
         if re.match(r'^su-.*@sitare\.org$', email):
             return redirect(url_for('dashboard'))
-        elif re.match(r'^(kpuneet474@gmail\.com)$', user_info['email']):
+        elif re.match(r'^(kpuneet474@gmail\.com|[a-zA-Z0-9._%+-]+@sitare\.org)$', user_info['email']):
             return redirect(url_for('teacher_portal'))
         elif re.match(r'^kronit747@gmail\.com$', email):
             return redirect(url_for('admin_portal'))
@@ -182,7 +182,7 @@ def dashboard():
 
     if re.match(r'^su-.*@sitare\.org$', user_info['email']):
         return render_template('Redirect_page.html')
-    elif re.match(r'^(kpuneet474@gmail\.com|kushal@sitare\.org|preeti@sitare\.org)$', user_info['email']):
+    elif re.match(r'^(kpuneet474@gmail\.com|[a-zA-Z0-9._%+-]+@sitare\.org)$', user_info['email']):
         return redirect(url_for('teacher_portal'))
     elif re.match(r'^krishu747@gmail\.com$', user_info['email']):
         return redirect(url_for('admin_portal'))
@@ -400,7 +400,7 @@ def calculate_rating_distributions(feedback_data):
 @app.route('/teacher_portal')
 def teacher_portal():
     user_info = session.get('user_info')
-    if not user_info or not re.match(r'^(kpuneet474@gmail\.com|kushal@sitare\.org|preeti@sitare\.org)$', user_info['email']):
+    if not user_info or not re.match(r'^(kpuneet474@gmail\.com|[a-zA-Z0-9._%+-]+@sitare\.org)$', user_info['email']):
         return redirect(url_for('login'))
 
     instructor_email = user_info['email']
@@ -715,9 +715,9 @@ def schedule_emails():
         time.sleep(60)  # wait one minute
 
 if __name__ == '__main__':
-     app.run(debug=True)
+     # app.run(debug=True)
 
-    # threading.Thread(target=schedule_emails, daemon=True).start()
-    # port = int(os.environ.get('PORT', 5000))
-    # app.run(host="0.0.0.0", port=port, debug=True)
+    threading.Thread(target=schedule_emails, daemon=True).start()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
