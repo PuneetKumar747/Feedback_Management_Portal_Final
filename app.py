0cfc598831261a015d331ff5b37ee78859f03658
Puneet kumar <143442578+PuneetKumar747@users.noreply.github.com>
2024-09-21 11:30:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -757,8 +757,10 @@ while True:
 if __name__ == '__main__':
     get_db_connection()
     create_tables_if_not_exists()
-    port = int(os.environ.get('PORT', 10000))
-    # app.run(host="0.0.0.0", port=port, debug=False)
-    app.run_server(debug=True, port=8050, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT from the environment or default to 5000
+    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get('PORT', 10000))
+    # # app.run(host="0.0.0.0", port=port, debug=False)
+    # app.run_server(debug=True, port=8050, host='0.0.0.0')
 
 
