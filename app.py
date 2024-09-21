37ec1b770bf460e465002d9f8c6324d2edf5dce6
Puneet kumar <143442578+PuneetKumar747@users.noreply.github.com>
2024-09-21 09:11:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -759,6 +759,6 @@ if __name__ == '__main__':
     create_tables_if_not_exists()
     # port = int(os.environ.get('PORT', 5000))
     # app.run(host="0.0.0.0", port=port, debug=False)
-    app.run(debug=True)
+    app.run(debug=True, port = 5000)
 
 
